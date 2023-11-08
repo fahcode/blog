11544d623d40aa9d942c219ce2ed0b44987ef0cf
fahuihuang <fahuihuang@tencent.com>
2023-11-08 11:54:45 +0800

    feat: add lerna

---
+++ packages/blog-admin/src/common/BaseImg.js
@@ -0,0 +1,142 @@
+import React from 'react'
+
+// Info
+// logo
+// visibility
+// visibilityOff
+// exit
+// reception
+// home
+// search
+// refresh
+// add
+// back
+// clear
+// like
+// comment
+// delete
+// image
+// menu
+
+export const Info = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+  </svg>
+)
+
+export const Logo = props => (
+  <img src={require('../assets/logo.jpg')} style={{ borderRadius: 50, overflow: 'hidden' }} width={100} height={100} {...props} alt="logo" />
+)
+
+export const VisibilityOff = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0z" fill="none" />
+    <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46A11.804 11.804 0 0 0 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z" />
+  </svg>
+)
+
+export const Visibility = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
+  </svg>
+)
+
+export const Exit = props => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}>
+    <path fill="none" d="M0 0h24v24H0z" />
+    <path d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z" />
+  </svg>
+)
+
+export const Reception = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5a2 2 0 0 0-2 2v4h2V5h14v14H5v-4H3v4a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z" />
+  </svg>
+)
+
+export const Home = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export const Search = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export const Refresh = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export const Add = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export const Back = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+  </svg>
+)
+
+export const Clear = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export const Like = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z" />
+  </svg>
+)
+
+export const Comment = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z" />
+  </svg>
+)
+
+export const Delete = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export const Update = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+)
+
+export const Image = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z" />
+  </svg>
+)
+
+export const Menu = props => (
+  <svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+  </svg>
+)
